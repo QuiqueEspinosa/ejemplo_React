cfa9578bc90c3fa98f78f24b99ce361696abb9e6
Enrique Espinosa <89802341+QuiqueEspinosa@users.noreply.github.com>
2023-08-15 09:44:11 -0300

    primer commit
    Enrique espinosa y Maximiliano Ferrez

---
+++ src/componente/cartel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './cartel.css'
+
+export default function Cartel() {
+    return (
+        <div class="name">
+            <h1>Maximiliano Ferrez <br/>y<br/> Enrique Espinosa</h1>
+        </div>
+    )
+}
